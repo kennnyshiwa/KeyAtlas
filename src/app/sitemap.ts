36d2ba7f8cdbf86ceb164e1ba7cffabc3eb596e9
kennnyshiwa <kenneth.stjohn@me.com>
2026-02-21 20:25:52 -0500

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,8 +2,10 @@ import type { MetadataRoute } from "next";
 import { prisma } from "@/lib/prisma";
 import { getSiteUrl } from "@/lib/site";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const siteUrl = getSiteUrl();
+  const siteUrl = getSiteUrl().replace(/\/$/, "");
 
   const staticRoutes: MetadataRoute.Sitemap = [
     "",
